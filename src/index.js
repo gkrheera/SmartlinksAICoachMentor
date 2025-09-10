ca52045cc7cc03276601ef6272f40dafaa819665
gkrheera <aortic-wisest-6s@icloud.com>
2025-09-10 14:00:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { PublicClientApplication } from '@azure/msal-browser';
-import { MsalProvider } from '@azure/msal-react';
-import { msalConfig } from './authConfig';
 import './index.css';
 import App from './App';
-
-const msalInstance = new PublicClientApplication(msalConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MsalProvider instance={msalInstance}>
-      <App />
-    </MsalProvider>
+    <App />
   </React.StrictMode>
 );
-
